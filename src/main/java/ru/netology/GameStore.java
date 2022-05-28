809cbfe09922b667f712cfcc47d460e3e480f4a2
Алексей Полетаев <Alexkok18@mail.ru>
2022-05-28 15:22:37 +0300

    fixes #1

--- src/main/java/ru/netology/GameStore.java
+++ src/main/java/ru/netology/GameStore.java
@@ -30,12 +30,8 @@ public class GameStore {
      * если игра есть и false иначе
      */
     public boolean containsGame(Game game) {
-        for (int i = 1; i < games.size(); i++) {
-            if (games.get(i - 1).equals(game)) {
-                return true;
-            }
-        }
-        return false;
+
+        return games.contains(game);
     }
 
     /**
